4d85c881d63cc7691ff69ff5d6faafd064ffc9c6
downson <downson.yuen@gmail.com>
2018-05-14 23:32:13 -0400

    Fix tensor shape issue & modify cnn

--- cnn_pdf.py
+++ cnn_pdf.py
@@ -4,18 +4,22 @@ Convolutional Neural Network model for PDF identification
 
 import tensorflow as tf
 import numpy as np
+import input_data
 
-PIXEL_DIMENSION_WIDTH = 28
-PIXEL_DIMENSION_HEIGHT = 28
+PIXEL_DIMENSION_WIDTH = 1828
+PIXEL_DIMENSION_HEIGHT = 1306
 FIRST_CONV_LAYER_FILTERS = 32
 DOWNSAMPLE_SIZE = 2
 KERNEL_SIZE = 5
+DENSE_NEURON_NUM = 1024
+LOGITS_NEURON_NUM = 2   # Number of target class (tablePositive, tableNegative)
 
 def cnn_pdf_model(features):
 
     # Input layer
     with tf.name_scope('reshape'):
         pdf_input = tf.reshape(features, [-1, PIXEL_DIMENSION_WIDTH, PIXEL_DIMENSION_HEIGHT, 1])
+        print(pdf_input)
 
     # First convolutional layer
     with tf.name_scope('first_conv'):
@@ -29,9 +33,16 @@ def cnn_pdf_model(features):
 
     # First dense layer
     with tf.name_scope('first_dense'):
-        w_dense = weight_variable([7*7*64,1024])
-        b_dense = bias_variable([1024])
-        pool_flat = tf.reshape(first_pool, [-1,7*7*64])
+        # first_pool_batch = first_pool.get_shape()[0]
+        print(first_pool.get_shape())
+        first_pool_width = tf.cast(first_pool.get_shape()[1], tf.int32)
+        first_pool_height = tf.cast(first_pool.get_shape()[2], tf.int32)
+        first_pool_channel = tf.cast(first_pool.get_shape()[3], tf.int32)
+
+        # first_pool_width*first_pool_height*first_pool_channel
+        w_dense = weight_variable([914*653*32, DENSE_NEURON_NUM])
+        b_dense = bias_variable([DENSE_NEURON_NUM])
+        pool_flat = tf.reshape(first_pool, [-1,first_pool_width*first_pool_height*first_pool_channel])
         dense = tf.nn.relu(tf.matmul(pool_flat,w_dense)+b_dense)
 
     # Dropout
@@ -41,14 +52,15 @@ def cnn_pdf_model(features):
 
     # Logits layer
     with tf.name_scope('logits'):
-        w_dense = weight_variable([1024,10])
-        b_dense = bias_variable([10])
+        w_dense = weight_variable([DENSE_NEURON_NUM,LOGITS_NEURON_NUM])
+        b_dense = bias_variable([LOGITS_NEURON_NUM])
         y_conv = tf.matmul(dropout, w_dense)+b_dense
 
     return y_conv, keep_prob
 
 def convolutional_2d_nn(input, weight):
-    return tf.nn.conv2d(input, weight, strides=[1, 1, 1, 1], padding='SAME')
+    float32_input = tf.cast(input, tf.float32)
+    return tf.nn.conv2d(float32_input, weight, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_n(value, n):
     return tf.nn.max_pool(value, ksize=[1,n,n,1], strides=[1,n,n,1], padding='SAME')
@@ -58,3 +70,6 @@ def weight_variable(shape):
 
 def bias_variable(shape):
     return tf.Variable(tf.constant(0.1, shape=shape))
+
+
+cnn_pdf_model(input_data.load_image())
--- input_data.py
+++ input_data.py
@@ -17,14 +17,15 @@ def load_image():
         print(img)
         print(list(img.getdata())[0])
         img_input.append(reshape_image(img))
-        print(img_input)
+        return reshape_image(img)
+        # print(len(reshape_image(img)))
 
-    return img_input
+    # return img_input
 
 def reshape_image(temp_image):
     # reshaped = np.array(temp_image.getdata()).reshape(temp_image.size[0], temp_image.size[1], 1)
     reshaped = np.array(temp_image.getdata())
-    # reshaped = reshaped.tolist()
+    reshaped = reshaped.tolist()
 
     return reshaped
 
